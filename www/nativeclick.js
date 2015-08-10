b2ea01c349ff6be657851bdb5dacf5b6cd66cf34
Matías Menich <matimenich@gmail.com>
2015-08-09 22:18:31 -0300

    new watch function

--- www/nativeclick.js
+++ www/nativeclick.js
@@ -1,19 +1,30 @@
-document.body.onclick = function(e){
-   e=window.event? event.srcElement: e.target;console.log(e);
-   if(e.className && e.className.indexOf('button')!=-1){
-   	cordova.exec(function () { },
-                function () { },
-                "SoundEffects",
-                "click",
-                []);
+var exec = require('cordova/exec');
 
-   }
-   else if(e.className && e.className.indexOf('sound-click')!=-1){
-   		cordova.exec(function () { },
-                function () { },
-                "SoundEffects",
-                "click",
-                []);
+var nativeclick = {
 
-   }
+	watch: function(successCallback, errorCallback, options){
+		document.body.onclick = function(e) {
+		   e=window.event? event.srcElement: e.target;console.log(e);
+		   if(e.className && e.className.indexOf('button')!=-1){
+		   		exec(
+		   			successCallback,
+		            errorCallback,
+		            "SoundEffects",
+		            "click",
+		            []);
+
+		   }
+		   else if(e.className && e.className.indexOf('sound-click')!=-1){
+		   		exec(
+		   			successCallback,
+		            errorCallback,
+		            "SoundEffects",
+		            "click",
+		            []);
+
+		   }
+		};
+	}
 };
+
+module.exports = nativeclick;
