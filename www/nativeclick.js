c54de9387ebd40e3ca85fcd9727a97a6395132d8
Matías Menich <matimenich@gmail.com>
2015-08-09 23:56:08 -0300

    removing unnecessary callbacks

--- www/nativeclick.js
+++ www/nativeclick.js
@@ -3,14 +3,19 @@ var exec = require('cordova/exec');
 var nativeclick = {
 
 	watch: function(classes){
+
+    var errorCallback = function(error){
+      console.error("Native Click Error:"+error);
+    };
+
 		document.body.onclick = function(e) {
 			e=window.event? event.srcElement: e.target;
 
-			classes = classes || [];
+			var clickyClasses = classes || [];
 
-			for(var i = 0; i < classes.length; i++) {
-				if(e.className && e.className.indexOf(classes[i]) != -1) {
-					exec(successCallback, errorCallback, "NativeClick", "click", options);
+			for(var i = 0; i < clickyClasses.length; i++) {
+				if(e.className && e.className.indexOf(clickyClasses[i]) != -1) {
+					exec(null, errorCallback, "NativeClick", "click", options);
 				}
 			}
 		};
