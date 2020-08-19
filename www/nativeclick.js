3811503fadfb059e7e5a75386d859bf9c2318e18
Mario Henrique <mariohenriquelopes3@gmail.com>
2020-08-19 10:23:33 -0300

    Watch By Tag Name

--- www/nativeclick.js
+++ www/nativeclick.js
@@ -17,6 +17,21 @@ var nativeclick = {
       });
     }    
   },
+  watchByTagName: function(tags){
+    var clickyTags = tags || [];
+
+    for(var i = 0; i < clickyTags.length; i++) {
+      watchTag(clickyTags[i]);
+    }
+
+    function watchTag(tag){
+        document.addEventListener('click', function(event) {
+        if (event.target.tagName == tag || event.target.parentElement.tagName == tag || event.target.parentElement.parentElement.tagName == tag || event.target.parentElement.parentElement.parentElement.tagName == tag) {
+          exec(null, errorCallback, "NativeClick", "click", []);
+        }
+      });
+    }    
+  },
 
   trigger: function () {
     exec(null, errorCallback, "NativeClick", "click", []);
